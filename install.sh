8ca4258ddd11cab9ab45e085c73e0fe86f8c1190
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-06-12 13:56:59 -0300

    Copy instalation from Gitpods official tutorial

--- install.sh
+++ install.sh
@@ -1,2 +1,30 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+# No need for the below command as the `home_files/.zshrc` already has the line.
+# echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# Symlink dotfiles
+current_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+dotfiles_source="${current_dir}/home_files"
+
+while read -r file; do
+
+    relative_file_path="${file#"${dotfiles_source}"/}"
+    target_file="${HOME}/${relative_file_path}"
+    target_dir="${target_file%/*}"
+
+    if test ! -d "${target_dir}"; then
+        mkdir -p "${target_dir}"
+    fi
+
+    printf 'Installing dotfiles symlink %s\n' "${target_file}"
+    ln -sf "${file}" "${target_file}"
+
+done < <(find "${dotfiles_source}" -type f)
+
+# Load bash environment in zsh
+# Taken from https://github.com/axonasif/bashenv.zsh
+echo "set +m; source <(bash -lic 'declare -px'); set -m" >> "$HOME/.zshrc"
