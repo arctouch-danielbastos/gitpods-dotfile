6c8ae8c7ba737a962d14cb70134f09bf4fdb213f
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-07-08 14:54:51 -0300

    Break up symlink.sh file

--- install.sh
+++ install.sh
@@ -12,25 +12,9 @@ sudo chsh -s $(which zsh) gitpod
 # Install neovim
 brew install neovim
 
-current_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+current_dir=$(dirname "$(realpath "$0")") 
 "${current_dir}/cleanUpTasks.sh"
-
-# Symlink dotfiles
-dotfiles_source="${current_dir}/home_files"
-
-while read -r file; do
-    relative_file_path="${file#"${dotfiles_source}"/}"
-    target_file="${HOME}/${relative_file_path}"
-    target_dir="${target_file%/*}"
-
-    if test ! -d "${target_dir}"; then
-        mkdir -p "${target_dir}"
-    fi
-
-    printf 'Installing dotfiles symlink %s\n' "${target_file}"
-    ln -sf "${file}" "${target_file}"
-
-done < <(find "${dotfiles_source}" -type f)
+"${current_dir}/symlink.sh"
 
 # Load bash environment in zsh
 # Taken from https://github.com/axonasif/bashenv.zsh
