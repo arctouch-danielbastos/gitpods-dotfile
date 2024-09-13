b0df1abe10a1e70ffd1b586162839211d3a7025a
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-09-13 10:11:02 -0300

    Add nvim

--- install.sh
+++ install.sh
@@ -8,13 +8,12 @@ sudo chsh -s $(which zsh) gitpod
 
 # echo alias gitpod=/usr/bin/gp > $HOME/.oh-my-zsh/custom/gitpod.zsh
 # env
+current_dir=$(dirname "$(realpath "$0")") 
 
 # Install neovim
 brew install neovim
-
-current_dir=$(dirname "$(realpath "$0")") 
 "${current_dir}/cleanUpTasks.sh"
-"${current_dir}/symlink.sh"
+ln -s $(pwd)/nvim $(HOME)/.nvim
 
 # Load bash environment in zsh
 # Taken from https://github.com/axonasif/bashenv.zsh
