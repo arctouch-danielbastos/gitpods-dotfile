a4aa529707362743b1e56c0db9aba4cc3a3baa5b
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-09-13 17:18:37 -0300

    Clean up nvim config

--- install.sh
+++ install.sh
@@ -6,14 +6,9 @@ echo "Setting up ohmyzsh powerlevel10k"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo chsh -s $(which zsh) gitpod
 
-# echo alias gitpod=/usr/bin/gp > $HOME/.oh-my-zsh/custom/gitpod.zsh
-# env
-current_dir=$(dirname "$(realpath "$0")") 
-
 # Install neovim
 brew install neovim
 "${curr ent_dir}/cleanUpTasks.sh"
-nvim --headless '+Lazy install'
 
 # Load bash environment in zsh
 # Taken from https://github.com/axonasif/bashenv.zsh
