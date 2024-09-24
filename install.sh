ddaffe6b35be024493a7b2c92fc179ae329a888f
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-09-24 10:51:12 -0300

    Trying something

--- install.sh
+++ install.sh
@@ -8,10 +8,6 @@ sudo chsh -s $(which zsh) gitpod
 brew install neovim
 current_dir=$(dirname "$(realpath "$0")") 
 
-# Load bash environment in zsh
-# Taken from https://github.com/axonasif/bashenv.zsh
-echo "set +m; source <(bash -lic 'declare -px'); set -m" >> "$HOME/.zshrc"
-
 # Ensure necessary directories exist
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.local/share/nvim"
@@ -28,3 +24,7 @@ echo "===== MOVING ZSH FILES"
 # Move or link the Zsh configuration files
 mv -f "$current_dir/zsh/.zshrc" "$HOME/.zshrc"
 mv -f "$current_dir/zsh/dbastos.zsh-theme" "$HOME/.oh-my-zsh/themes/dbastos.zsh-theme"
+
+# Load bash environment in zsh
+# Taken from https://github.com/axonasif/bashenv.zsh
+echo "set +m; source <(bash -lic 'declare -px'); set -m" >> "$HOME/.zshrc"
