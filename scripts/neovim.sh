29d11d27ac13abfe28eb0d861d800e06c5a73338
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-09-24 19:10:32 -0300

    Fix dotfile find placement

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -2,8 +2,7 @@
 
 # Install neovim
 brew install neovim > /dev/null
-current_dir=$(dirname "$(realpath "$0")") 
-printf 'current dir is %s\n'  "$current_dir"
+dotfile_dir=~/.dotfiles
 
 # Ensure necessary directories exist
 mkdir -p "$HOME/.config/nvim"
@@ -12,6 +11,6 @@ mkdir -p "$HOME/.local/share/nvim"
 printf "===== MOVING NEOVIM FILES"
 
 # Move or link the Neovim configuration files
-mv -f "$current_dir/nvim/init.lua" "$HOME/.config/nvim/init.lua"
-mv -f "$current_dir/nvim/lazy" "$HOME/.local/share/nvim/lazy"
-mv -f "$current_dir/nvim/lazy-lock.json" "$HOME/.config/nvim/lazy-lock.json"
+mv -f "$dotfile_dir/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+mv -f "$dotfile_dir/nvim/lazy" "$HOME/.local/share/nvim/lazy"
+mv -f "$dotfile_dir/nvim/lazy-lock.json" "$HOME/.config/nvim/lazy-lock.json"
