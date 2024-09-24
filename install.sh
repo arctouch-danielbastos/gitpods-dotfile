3fc9fd37cbeb3d0f16c05a0be168ad29a9905c42
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-09-24 10:29:26 -0300

    Add echos for debugging

--- install.sh
+++ install.sh
@@ -17,11 +17,14 @@ mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.local/share/nvim"
 mkdir -p "$HOME/.oh-my-zsh/themes"
 
+echo "===== MOVING NEOVIM FILES"
+
 # Move or link the Neovim configuration files
 mv "$current_dir/nvim/init.lua" "$HOME/.config/nvim/init.lua"
 mv "$current_dir/nvim/lazy" "$HOME/.local/share/nvim/lazy"
 mv "$current_dir/nvim/lazy-lock.json" "$HOME/.config/nvim/lazy-lock.json"
 
+echo "===== MOVING ZSH FILES"
 # Move or link the Zsh configuration files
 mv "$current_dir/zsh/.zshrc" "$HOME/.zshrc"
 mv "$current_dir/zsh/dbastos.zsh-theme" "$HOME/.oh-my-zsh/themes/dbastos.zsh-theme"
