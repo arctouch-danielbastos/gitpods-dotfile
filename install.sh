765381d47b1dfa953a19651c95eb54dc72859513
arctouch-danielbastos <daniel.bastos@quizlet.com>
2024-09-24 11:30:02 -0300

    Last try

--- install.sh
+++ install.sh
@@ -7,25 +7,26 @@ sudo chsh -s $(which zsh) gitpod
 # Install neovim
 brew install neovim
 current_dir=$(dirname "$(realpath "$0")") 
+printf 'current dir is %s\n'  "$current_dir"
 
 # Ensure necessary directories exist
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.local/share/nvim"
 mkdir -p "$HOME/.oh-my-zsh/themes"
 
-echo "===== MOVING NEOVIM FILES"
+printf "===== MOVING NEOVIM FILES"
 
 # Move or link the Neovim configuration files
-ln -sf "$current_dir/nvim/init.lua" "$HOME/.config/nvim/init.lua"
-ln -sf "$current_dir/nvim/lazy" "$HOME/.local/share/nvim/lazy"
-ln -sf "$current_dir/nvim/lazy-lock.json" "$HOME/.config/nvim/lazy-lock.json"
+mv -f "$current_dir/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+mv -f "$current_dir/nvim/lazy" "$HOME/.local/share/nvim/lazy"
+mv -f "$current_dir/nvim/lazy-lock.json" "$HOME/.config/nvim/lazy-lock.json"
 
-echo "===== MOVING ZSH FILES"
+printf "===== MOVING ZSH FILES"
 
 # Move or link the Zsh configuration files
-ln -sf "$current_dir/zsh/.zshrc" "$HOME/.zshrc"
-ln -sf "$current_dir/zsh/dbastos.zsh-theme" "$HOME/.oh-my-zsh/themes/dbastos.zsh-theme"
+mv -f "$current_dir/zsh/.zshrc" "$HOME/.zshrc"
+mv -f "$current_dir/zsh/dbastos.zsh-theme" "$HOME/.oh-my-zsh/themes/dbastos.zsh-theme"
 
 # Load bash environment in zsh
 # Taken from https://github.com/axonasif/bashenv.zsh
-echo "set +m; source <(bash -lic 'declare -px'); set -m" >> "$HOME/.zshrc"
+printf "set +m; source <(bash -lic 'declare -px'); set -m" >> "$HOME/.zshrc"
